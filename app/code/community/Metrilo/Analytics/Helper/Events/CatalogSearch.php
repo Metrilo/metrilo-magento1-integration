3b931188eb8e880bece1fdf0fcef75a2de661c74
Ned <ned@metrilo.com>
2019-10-30 13:22:31 +0200

    PLG-147: Added advanced_search as string query param when advanced search is being used

--- app/code/community/Metrilo/Analytics/Helper/Events/CatalogSearch.php
+++ app/code/community/Metrilo/Analytics/Helper/Events/CatalogSearch.php
@@ -2,6 +2,7 @@
 class Metrilo_Analytics_Helper_Events_CatalogSearch extends Mage_Core_Helper_Abstract
 {
     public function callJS() {
-        return "window.metrilo.search('" . Mage::helper('catalogsearch')->getQueryText() . "', '" . Mage::helper('core/url')->getCurrentUrl() . "');";
+        $query = (Mage::helper('catalogsearch')->getQueryText()) ? Mage::helper('catalogsearch')->getQueryText() : 'advanced_search';
+        return "window.metrilo.search('" . $query . "', '" . Mage::helper('core/url')->getCurrentUrl() . "');";
     }
 }
