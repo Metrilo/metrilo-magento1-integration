3e0aa5d5cac2393817cbe8663162c177cd71f279
Ned <ned@metrilo.com>
2020-12-14 11:39:51 +0200

    KYP-1569: Updated admin message for default configuration scope settings save

--- app/code/community/Metrilo/Analytics/Model/ConfigObserver.php
+++ app/code/community/Metrilo/Analytics/Model/ConfigObserver.php
@@ -13,10 +13,22 @@ class Metrilo_Analytics_Model_ConfigObserver extends Varien_Event_Observer
     public function saveConfig($observer)
     {
         try {
-            if (!$this->_activityHelper->createActivity($observer->getStore(), 'integrated')) {
-                Mage::getSingleton('core/session')
-                    ->addError('The API Token and/or API Secret you have entered are invalid.
-                     You can find the correct ones in Settings -> Installation in your Metrilo account.');
+            $storeId = $observer->getStore();
+            if (!$this->_activityHelper->createActivity($storeId, 'integrated')) {
+                if ($storeId === 0) {
+                    Mage::getSingleton('core/session')->addError(
+                        'You\'ve just entered the API token and API Secret to the default configuration scope .
+                        This means that the Metrilo module will be added to all your store views .
+                        If you want to connect only a specific store view, please remove it form the default scope and
+                        add it only to the specific store view configuration scope .
+                        You can find the "Import" button by opening any specific configuration scope .'
+                    );
+                } else {
+                    Mage::getSingleton('core/session')->addError(
+                        'The API Token and/or API Secret you have entered are invalid.
+                        You can find the correct ones in Settings -> Installation in your Metrilo account.'
+                    );
+                }
             }
         } catch (Exception $e) {
             $this->_helper->logError('ConfigObserver', $e);
